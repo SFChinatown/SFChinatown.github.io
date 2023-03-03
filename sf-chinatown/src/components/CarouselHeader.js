de1a15d140393edbfcb43aeaa2a75b774cc3ed0e
Lily Yang <lilyyang@Lilys-MacBook-Pro.local>
2023-03-02 17:47:10 -0800

    fixed sizing

--- sf-chinatown/src/components/CarouselHeader.js
+++ sf-chinatown/src/components/CarouselHeader.js
@@ -5,22 +5,24 @@ import './CarouselHeader';
 function CarouselHeader(props) {
   const carouselItems = props.items.map((slide) =>
     <Carousel.Item>
+      <Carousel.Caption>
+        <h3>{slide.label}</h3>
+        <p>{slide.caption}</p>
+      </Carousel.Caption>
       <img
         className="d-block w-100"
         src={require(`../assets/images/${slide.path}`)}
         alt={slide.alt}
       />
-      <Carousel.Caption>
-        <h3>{slide.label}</h3>
-        <p>{slide.caption}</p>
-      </Carousel.Caption>
     </Carousel.Item>
   )
 
   return (
-    <Carousel variant="dark">
-      {carouselItems}
-    </Carousel>
+    <div>
+      <Carousel variant="light">
+        {carouselItems}
+      </Carousel>
+    </div>
   );
 }
 
