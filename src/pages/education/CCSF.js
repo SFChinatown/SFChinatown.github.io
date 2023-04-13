b65e3c8446d1e488d125dcd19ad34db66b8c50de
Lily Yang <lily.eyang@gmail.com>
2023-04-13 15:15:56 -0700

    aux sites set up

---
+++ src/pages/education/CCSF.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import "./CCSF.css";
+
+const CCSF = () => {
+    return (
+        <Footer />
+    )
+}
+
+export default CCSF
---
+++ src/pages/public-health/ChineseHospital.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import "./ChineseHospital.css";
+
+const ChineseHospital = () => {
+    return (
+        <Footer />
+    )
+}
+
+export default ChineseHospital
