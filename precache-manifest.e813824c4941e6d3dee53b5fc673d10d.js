7badc306913f85a48fead37fc23bab1d1fa30f70
Lily Yang <lily.eyang@gmail.com>
2023-04-20 12:02:26 -0700

    Updates

---
+++ precache-manifest.e813824c4941e6d3dee53b5fc673d10d.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b2a928b0911339ce619",
+    "url": "/SFChinatown.github.io/static/css/main.63a9e259.chunk.css"
+  },
+  {
+    "revision": "6b2a928b0911339ce619",
+    "url": "/SFChinatown.github.io/static/js/main.6b2a928b.chunk.js"
+  },
+  {
+    "revision": "9e6c96c48f2fd00bc38b",
+    "url": "/SFChinatown.github.io/static/js/1.9e6c96c4.chunk.js"
+  },
+  {
+    "revision": "17e256872c6fdacf17a4",
+    "url": "/SFChinatown.github.io/static/css/2.9f8f267d.chunk.css"
+  },
+  {
+    "revision": "17e256872c6fdacf17a4",
+    "url": "/SFChinatown.github.io/static/js/2.17e25687.chunk.js"
+  },
+  {
+    "revision": "4f60dfb6b3b858c9cdc6",
+    "url": "/SFChinatown.github.io/static/js/runtime~main.4f60dfb6.js"
+  },
+  {
+    "revision": "ace6394f3026893ba07f32faed7c905b",
+    "url": "/SFChinatown.github.io/static/media/test.ace6394f.png"
+  },
+  {
+    "revision": "7c358a06fecc9fbcd2bf20b5e31396ee",
+    "url": "/SFChinatown.github.io/static/media/CCSF_1.7c358a06.JPG"
+  },
+  {
+    "revision": "d657def08091288e787e96f51bd3b88d",
+    "url": "/SFChinatown.github.io/static/media/CCSF_2.d657def0.JPG"
+  },
+  {
+    "revision": "3803636177d23ef6669c25fee0948e2f",
+    "url": "/SFChinatown.github.io/static/media/CCSF_3.38036361.JPG"
+  },
+  {
+    "revision": "58d793e7543002b78efa3c55c25d0792",
+    "url": "/SFChinatown.github.io/static/media/CAA_1.58d793e7.JPG"
+  },
+  {
+    "revision": "3b46c9720eac14e9bebd6ee332325697",
+    "url": "/SFChinatown.github.io/static/media/CAA_2.3b46c972.JPG"
+  },
+  {
+    "revision": "06d0ac2c288411f2430ac1b203520b4d",
+    "url": "/SFChinatown.github.io/static/media/CAA_3.06d0ac2c.JPG"
+  },
+  {
+    "revision": "e5a9b7ac6db97b3521ab83fcce1933b3",
+    "url": "/SFChinatown.github.io/static/media/CAA_4.e5a9b7ac.JPG"
+  },
+  {
+    "revision": "9480fd0b5798cc5b3eeed846bb22a710",
+    "url": "/SFChinatown.github.io/static/media/CAA_5.9480fd0b.JPG"
+  },
+  {
+    "revision": "ae1c99fefff9bad66c0bf8d8cc393489",
+    "url": "/SFChinatown.github.io/static/media/CAA_6.ae1c99fe.JPG"
+  },
+  {
+    "revision": "aeb78ad4d10c15a0911b8eae81f727b0",
+    "url": "/SFChinatown.github.io/static/media/CAA_7.aeb78ad4.JPG"
+  },
+  {
+    "revision": "7f970ad90a0204a35b20b9414f82a291",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_1.7f970ad9.jpeg"
+  },
+  {
+    "revision": "85b2a23b8c7a035b561be2cfa2d1c036",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_2.85b2a23b.jpg"
+  },
+  {
+    "revision": "76c923312f0f0c8865e60ce03e0ed933",
+    "url": "/SFChinatown.github.io/static/media/header.76c92331.svg"
+  },
+  {
+    "revision": "684b42d8f6f4e577229d61e6bffc695c",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_10.684b42d8.jpg"
+  },
+  {
+    "revision": "1a7cdeabfc24075df4dcd51b244ef248",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_3.1a7cdeab.png"
+  },
+  {
+    "revision": "1965e23cb4946929bfea032918f94f75",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_4.1965e23c.jpg"
+  },
+  {
+    "revision": "337044222181d39f6676f4072b49e4f0",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_5.33704422.jpeg"
+  },
+  {
+    "revision": "1f0d53c7fc25b39d2ad9e72a5dab760b",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_6.1f0d53c7.jpeg"
+  },
+  {
+    "revision": "7c60ae1a2f945160918dc70df6240e0d",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_7.7c60ae1a.png"
+  },
+  {
+    "revision": "069f9b86d4c2168462bea9ada84869ca",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_8.069f9b86.jpg"
+  },
+  {
+    "revision": "f6997273eaff36d8633d0b0550fa6727",
+    "url": "/SFChinatown.github.io/static/media/I-Hotel_9.f6997273.png"
+  },
+  {
+    "revision": "8ff9d496734bade4aad712594ea1b395",
+    "url": "/SFChinatown.github.io/static/media/maybe.8ff9d496.svg"
+  },
+  {
+    "revision": "4314d1521d1fd24d57017695157026ca",
+    "url": "/SFChinatown.github.io/static/media/logo.4314d152.svg"
+  },
+  {
+    "revision": "2d1ac8d90cee406437f84bd0644e9bfd",
+    "url": "/SFChinatown.github.io/index.html"
+  }
+];
