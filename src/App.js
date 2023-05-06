117472606e43a81f0085031700acc9e3e0d1ffbd
Lily Yang <lily.eyang@gmail.com>
2023-05-06 10:07:51 -0700

    map

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Theme from './pages/Theme';
-import MapPage from './pages/MapPage';
-// import Map from './pages/Map';
+import Map from './pages/Map';
 
 // Education pages
 import CCSF from './pages/education/CCSF';
@@ -40,7 +39,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/about-us" element={<About />}></Route>
-          {/* <Route exact path="/interactive-map" element={<MapPage />}></Route> */}
+          <Route exact path="/interactive-map" element={<Map />}></Route>
           <Route exact path="/main-themes" element={<Theme />}></Route>
 
           {/* Education */}
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,121 +1,73 @@
-// // render directory 
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-// if (this.readyState == 4 && this.status == 200) {
-//     var jObj = JSON.parse(this.responseText);
-//     //console.log("Json parsed data is: " + JSON.stringify(jObj));
-//     let text = "<div class='row'><h3 class='my-4'>DIRECTORY</h3>"
-//     for (let x in jObj) {
-//       text += "<h4 class='my-3'>" + jObj[x].CATOGERY + "</h4>";
-//       s = jObj[x].SITES
-//       for (let y in s) {
-//         text += "<div class='col-sm-3'>"
-//         text += "<p>" + s[y].NAME + "<br>" + s[y].STREET+ "<br>" + s[y].CITY + "<br>"
-//         if ( s[y].WEBSITE != '' ) {
-//             text += "<a href='" + s[y].WEBSITE + "'>Link to Website</a>"
-//         }
-//         text += "</div>"
-//       }
-//     }
-//     text += "</div>"
-//     document.getElementById("dirContainer").innerHTML = text;
-//   }
-// };
-// xmlhttp.open("GET", "Map.json", true);
-// xmlhttp.send();
+import React from 'react';
+import Footer from '../components/Footer';
+import "./Map.css";
 
-// // render map
-// xhr = new XMLHttpRequest();
-// xhr.open("GET","sfcmap.min.svg",false);
-// // Following line is just to be on the safe side;
-// // not needed if your server delivers SVG with correct MIME type
-// xhr.overrideMimeType("image/svg+xml");
-// xhr.onload = function(e) {
-//   // You might also want to check for xhr.readyState/xhr.status here
-//   document.getElementById("svgContainer")
-//     .appendChild(xhr.responseXML.documentElement);
-// };
-// xhr.send("");
+const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <title>SF Chinatown</title>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Rowdies:wght@700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- Bootstrap core JS-->
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+        <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
-// const outlineList = document.querySelectorAll('g[id$="Outline"]');
-// for (const i of outlineList.values()) { i.style='display:none'; }
+    </head>
+    <body>
 
-// var sites= { 
-//   '_41Ross': ['','a collaborative art space founded by CCDC and the Chinese Culture Center']
-// , 'Betty_Ann_Ong_Recreation_Center': ['','a state-of-the-art recreational facility']
-// , 'Cameron_House': ['','building strength and resilience through family-centered programs']
-// , 'Chinatown_Community_Development_Center': ['','strengthening community and enhancing quality of life in Chinatown']
-// , 'Chinese_for_Affirmative_Action': ['#f7b95e','a community based civil rights organization']
-// , 'Chinese_Hospital': ['#94cdd3','a full-service hospital providing compassionate, patient-centered healthcare']
-// , 'Chinese_Culture_Center': ['','a cultural anchor committed to racial equality through education and contemporary art']
-// , 'Chinese_Historical_Society_of_America': ['','collecting, preserving, and illuminating the history of Chinese in America']
-// , 'Chinese_Newcomers_Service_Center': ['','a multilingual agency providing transitional guidance and empowering immigrants']
-// , 'Chinese_Progressive_Association': ['','promoting collective power to demand better living and working conditions for all']
-// , 'City_College_of_SF_-_Chinatown': ['#f7b95e','empowering students with life skills, social knowledge, and self-confidence']
-// , 'Gordon_J_Lau_Elementary_School': ['','established in 1885 as the "Chinese Primary School," continues to provide education for local residents']
-// , 'Gum_Moon_Residence_Hall': ['#f07b3b','providing sanctuary for women and children in geographic and social transition']
-// , 'International_Hotel': ['#f07b3b','historical site of landmark affordable housing advocacy in Chinatown']
-// , 'Kubla_Khan_Nightclub': ['#cf2844','once a lively cabaret nightclub in the 1940s, now a fine arts shop']
-// , 'Mei_Lun_Yuen': ['#f07b3b','a 185-unit complex constructed under the HUD 236 Program']
-// , 'North_Ping_Yuen': ['','1961 annex to Ping Yuen, adding 185 additional units']
-// , 'Ping_Yuen': ['#f07b3b','landmark housing project, providing 234-units to low-income families']
-// , 'Portsmouth_Square': ['#95d1a3','Popular open space public park, dating back to Spanish California']
-// , 'Self-Help_for_the_Elderly_-_Geen_Mun': ['','providing support to seniors in order to promote independence, dignity, and self-worth']
-// , 'Spofford_Alley': ['#f07b3b','historic alley of Chinatown, one of the many involved in the Alleyways Program']
-// , 'St_Mary_s_Square': ['','a rooftop green space located on top of a multi-level garage']
-// , 'West_Ping_Yuen': ['','an addition to Central Ping Yuen completed in 1956']
-// , 'Willie_Wong_Playground': ['#95d1a3','an urban park featuring playgrounds, athletic courts, and a gym']
-// , 'Woh_Hei_Yuen_Park': ['#95d1a3','a small urban park with a robust recreation center offering a wide range of programs']
-// , 'YMCA_Chinatown': ['','founded in 1911, serving the community as a recreation center and social site']
-// }
 
-// $.each(sites,
-// function( key, vals) {
-//   if(vals[0] != '') {
-//     $( "#"+key+"_Site" ).parent().hover( function() { $( "#"+key+"_Outline" ).css("display", "inline"); }, function() { $( "#"+key+"_Outline" ).css("display", "none"); });
+    <div id="intro"></div>
+    <!--Main layout-->
+    <main class="my-5">
+        <div class="container">
 
-//     $("head").append('<style type="text/css"></style>');
-//     var hbg = $("head").children(':last');
-//     hbg.html('.hbg'+key+'{--bs-popover-header-color:#ffffff;--bs-popover-header-bg:'+vals[0]+';}');
-//   } else {
-//     $("head").append('<style type="text/css"></style>');
-//     var hbg = $("head").children(':last');
-//     hbg.html('.hbg'+key+'{--bs-popover-header-bg:#ffffff;}');
-//     $("#"+key+"_Site").parent().css('cursor','default')
-//   }
+        <!--Section: Map -->
+        <section class="pb-4 mb-5">
+            <div class="row">
 
-//   new bootstrap.Popover($("#"+key+"_Site").parent(), { trigger:'hover', placement:'top', title : $("#"+key+"_Site").attr('data-name').replace(' Site', ''), html:true, content: '<i>'+ vals[1] +'</i>', container: 'body', customClass:'hbg'+key })
-// });
+        <!--- div class="text-center">
+        <br><h1 class="mb-3 h2 red">Interactive Map</h1>
+        <p class="mb-3">Hover over the buildings to explore</p>
+        </div --->
 
-// var beforePan
+    <div class="col-sm-1 mx-auto"></div>
+    <div class="col-sm-10 mx-auto">
 
-// beforePan = function(oldPan, newPan){
-          
-// var stopHorizontal = false
-//   , stopVertical = false
-//   , gutterWidth = 600 
-//   , gutterHeight = 600 
-// // Computed variables
-//   , sizes = this.getSizes()
-//   , leftLimit = -((sizes.viewBox.x + sizes.viewBox.width) * sizes.realZoom) + gutterWidth
-//   , rightLimit = sizes.width - gutterWidth - (sizes.viewBox.x * sizes.realZoom)
-//   , topLimit = -((sizes.viewBox.y + sizes.viewBox.height) * sizes.realZoom) + gutterHeight
-//   , bottomLimit = sizes.height - gutterHeight - (sizes.viewBox.y * sizes.realZoom)
+    <!--- copy sfcmap.min.svg, sfcmap.json, sfcmap.css, sfcmap.js into the template directory and insert the following chunk of html in page --->
+    <script src="https://cdn.jsdelivr.net/npm/svg-pan-zoom@3.6.1/dist/svg-pan-zoom.min.js"></script>
+    <link href="Map.css" rel="stylesheet" />
+    <div id="svgContainer" style="width: 920px; height: 600px; background-color: #eeece7"></div>
+    <div id='dirContainer'></div>
+    <script src="Map.js"></script>
+    <!--- end of map --->
+        
+        
+    </div>
 
-//   customPan = {}
-//   customPan.x = Math.max(leftLimit, Math.min(rightLimit, newPan.x))
-//   customPan.y = Math.max(topLimit, Math.min(bottomLimit, newPan.y))
+    <div class="col-sm-1 mx-auto"></div>
 
-//   return customPan
-// }
+            </div>
+        </section>
+        <!--Section: Map -->
 
-// svgPanZoom('#SFChinatown2023', {
-//   zoomEnabled: true
-//   , controlIconsEnabled: true
-//   , fit: 1
-//   , center: 1
-//   , beforePan: beforePan
-//   , minZoom: 1
-//   , maxZoom: 10
-//   , dblClickZoomEnabled: false
-// });
+        </div>
+    </main>
+    <!--Main layout-->
+
+
+
+    </body>
+    </html>
+`;
+const Map = () => {
+    return (
+        <iframe src="https://sfchinatown.github.io/map/"></iframe>
+    )
+}
+
+export default Map;
